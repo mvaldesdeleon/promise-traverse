ba802a3c27b615897e383b9e7313cb395f308269
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-05-14 00:28:55 +0200

    Adds tests.

---
+++ test/index.js
@@ -0,0 +1,30 @@
+const test = require('tape');
+const traverse = require('../index.js');
+
+test('traverse', t => {
+    const x = {
+        name: Promise.resolve('john'),
+        cats: [{color: 'white'}, {color: Promise.resolve('black')}],
+        numbers: [1, 13, Promise.resolve(42)]
+    };
+    const y = {
+        name: Promise.resolve('john'),
+        cats: [{color: 'white'}, {color: Promise.resolve('black')}],
+        numbers: [Promise.reject('boom'), 13, Promise.resolve(42)]
+    };
+
+    const a = traverse(x);
+    const b = traverse(y);
+
+    const aa = a.then(px => {
+        t.deepEqual({
+            name: 'john',
+            cats: [{color: 'white'}, {color: 'black'}],
+            numbers: [1, 13, 42]
+        }, px);
+    });
+
+    const bb = b.catch(err => t.equal('boom', err));
+
+    Promise.all([aa, bb]).then(_ => t.end());
+});
